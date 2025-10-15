d5664d65098d456f857129b1fd4c98190876a256
ValentinaStan <valentina.v_s@yahoo.com>
2025-10-15 21:11:46 +0200

    Actualizar API_URL y cambios frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5000/api"
-    : "https://coffee-manager-6ghx.onrender.com";
+// Definimos la URL de la API según entorno
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 // Auth
 export const loginUser = (username, password) =>
